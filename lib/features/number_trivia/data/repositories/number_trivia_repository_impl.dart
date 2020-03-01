aef9161a1ac86fffd5cec1d723d600439ec192ca
renanboni <renan.boni@usp.br>
2020-03-01 15:16:47 +0100

    Created abstractions for remote and local data sources

---
+++ lib/features/number_trivia/data/repositories/number_trivia_repository_impl.dart
@@ -0,0 +1,29 @@
+import 'package:dartz/dartz.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:trivia_number/core/error/failures.dart';
+import 'package:trivia_number/core/platform/network_info.dart';
+import 'package:trivia_number/features/number_trivia/data/datasources/number_trivia_local_datasource.dart';
+import 'package:trivia_number/features/number_trivia/data/datasources/number_trivia_remote_datasource.dart';
+import 'package:trivia_number/features/number_trivia/domain/entities/number_trivia.dart';
+import 'package:trivia_number/features/number_trivia/domain/repositories/number_trivia_repository.dart';
+
+class NumberTriviaRepositoryImpl implements NumberTriviaRepository {
+  final NumberTriviaRemoteDataSource remoteDataSource;
+  final NumberTriviaLocalDataSource localDataSource;
+  final NetworkInfo networkInfo;
+
+  NumberTriviaRepositoryImpl(
+      {@required this.remoteDataSource,
+      @required this.localDataSource,
+      @required this.networkInfo});
+
+  @override
+  Future<Either<Failure, NumberTrivia>> getConcreteNumberTrivia(int number) {
+    return null;
+  }
+
+  @override
+  Future<Either<Failure, NumberTrivia>> getRandomNumberTrivia() {
+    return null;
+  }
+}
